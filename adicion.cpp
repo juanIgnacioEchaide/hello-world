12bcc4f54aa58a04f7dc45bb35ce153c722c8e80
juanIgnacioEchaide <43832189+juanIgnacioEchaide@users.noreply.github.com>
2019-04-24 13:02:35 -0300

    Create adicion.cpp

---
+++ adicion.cpp
@@ -0,0 +1,19 @@
+/* AED
+	K1051
+	2019
+	169040-1
+	Juan Ignacio Echaide	
+	GitHub juanIgnacioEchaide
+	Trabajo Práctico #1 "Adición"
+*/
+
+#include <iostream>
+
+int main(){
+
+	double a, b;
+	std::cin >> a;
+	std::cin >> b;
+	std::cout << a+b;
+
+}
